2e693de825c30dd1be800e058181ab4428718dbe
thiagovndantas <thiagovndantas@gmail.com>
2023-04-20 18:23:20 -0300

    Alteração para pastas locais

--- main.py
+++ main.py
@@ -14,22 +14,22 @@ df1 = pd.DataFrame(loadShape1)
 df1.to_csv('loadshape1.csv', index=False, header=False)
 
 # simulação 1 - carga sem nada
-dss_file1 = "D:/OpenDSS/POLI_storage_pvsyst_simulation/simulacao1.dss"
+dss_file1 = "simulacao1.dss"
 
 # simulação 2 - carga somente com storage em mode = default
-dss_file2 = "D:/OpenDSS/POLI_storage_pvsyst_simulation/simulacao2.dss"
+dss_file2 = "simulacao2.dss"
 
 # simulação 3 - carga somente com storage em mode = follow
-dss_file3 = "D:/OpenDSS/POLI_storage_pvsyst_simulation/simulacao3.dss"
+dss_file3 = "simulacao3.dss"
 
 # simulação 4 - carga somente com storage em mode = peakshave
-dss_file4 = "D:/OpenDSS/POLI_storage_pvsyst_simulation/simulacao4.dss"
+dss_file4 = "simulacao4.dss"
 
 # simulação 5 - carga somente com pvsystem
-dss_file5 = "D:/OpenDSS/POLI_storage_pvsyst_simulation/simulacao5.dss"
+dss_file5 = "simulacao5.dss"
 
 # simulação 4 - carga somente com storage em modo = follow e pvsystem
-dss_file6 = "D:/OpenDSS/POLI_storage_pvsyst_simulation/simulacao6.dss"
+dss_file6 = "simulacao6.dss"
 
 # rodando os arquivos
 dss.text("compile {}".format(dss_file1))
@@ -41,17 +41,18 @@ dss.text("compile {}".format(dss_file6))
 
 # arquivos gerados pela compilação do código
 
-sim1_power_line = "D://OpenDSS/POLI_storage_pvsyst_simulation/fonte_Mon_monitor_power_line_sim1_1.csv"
-sim2_power_line = "D:/OpenDSS/POLI_storage_pvsyst_simulation/fonte_Mon_monitor_power_line_sim2_1.csv"
-sim2_power_batery = "D:/OpenDSS/POLI_storage_pvsyst_simulation/fonte_Mon_monitor_power_batery_sim2_1.csv"
-sim3_power_line = "D:/OpenDSS/POLI_storage_pvsyst_simulation/fonte_Mon_monitor_power_line_sim3_1.csv"
-sim3_power_batery = "D:/OpenDSS/POLI_storage_pvsyst_simulation/fonte_Mon_monitor_power_batery_sim3_1.csv"
-sim4_power_line = "D:/OpenDSS/POLI_storage_pvsyst_simulation/fonte_Mon_monitor_power_line_sim4_1.csv"
-sim4_power_batery = "D:/OpenDSS/POLI_storage_pvsyst_simulation/fonte_Mon_monitor_power_batery_sim4_1.csv"
-sim5_power_line = "D:/OpenDSS/POLI_storage_pvsyst_simulation/fonte_Mon_monitor_power_line_sim5_1.csv"
-sim6_power_line = "D:/OpenDSS/POLI_storage_pvsyst_simulation/fonte_Mon_monitor_power_line_sim6_1.csv"
-sim6_power_line2 = "D:/OpenDSS/POLI_storage_pvsyst_simulation/fonte_Mon_monitor_power_line2_sim6_1.csv"
-sim6_power_batery = "D:/OpenDSS/POLI_storage_pvsyst_simulation/fonte_Mon_monitor_power_batery_sim6_1.csv"
+sim1_power_line = "fonte_Mon_monitor_power_line_sim1_1.csv"
+sim2_power_line = "fonte_Mon_monitor_power_line_sim2_1.csv"
+sim2_power_batery = "fonte_Mon_monitor_power_batery_sim2_1.csv"
+sim3_power_line = "fonte_Mon_monitor_power_line_sim3_1.csv"
+sim3_power_batery = "fonte_Mon_monitor_power_batery_sim3_1.csv"
+sim4_power_line = "fonte_Mon_monitor_power_line_sim4_1.csv"
+sim4_power_batery = "fonte_Mon_monitor_power_batery_sim4_1.csv"
+sim5_power_line = "fonte_Mon_monitor_power_line_sim5_1.csv"
+sim6_power_line = "fonte_Mon_monitor_power_line_sim6_1.csv"
+sim6_power_line2 = "fonte_Mon_monitor_power_line2_sim6_1.csv"
+sim6_power_batery = "fonte_Mon_monitor_power_batery_sim6_1.csv"
+sim6_power_pv = "fonte_Mon_monitor_power_pv_sim6_1.csv"
 
 # criando os dataframes
 
@@ -67,14 +68,22 @@ df_sim5_power_line = pd.read_csv(sim5_power_line)
 df_sim6_power_line = pd.read_csv(sim6_power_line)
 df_sim6_power_line2 = pd.read_csv(sim6_power_line2)
 df_sim6_power_batery = pd.read_csv(sim6_power_batery)
+df_sim6_power_pv = pd.read_csv(sim6_power_pv)
 
 
 # plotando o gráfico
 
 plt.plot(df_sim1_power_line['hour'],
          df_sim1_power_line[' P1 (kW)'], label='Simulacao 1')
-plt.plot(df_sim4_power_line['hour'],
-         df_sim4_power_line[' P1 (kW)'], label='Simulacao 4')
+plt.plot(df_sim3_power_line['hour'],
+         df_sim3_power_line[' P1 (kW)'], label='Simulacao 6 - Linha 1')
+plt.plot(df_sim6_power_line2['hour'],
+         df_sim6_power_line2[' P1 (kW)'], label='Simulacao 6 - Linha 2')
+plt.plot(df_sim6_power_batery['hour'],
+         df_sim6_power_batery[' P1 (kW)'], label='Simulacao 6 - Bateria')
+plt.plot(df_sim6_power_pv['hour'],
+         df_sim6_power_pv[' P1 (kW)'], label='Simulacao 6 - PV')
+
 
 plt.xlabel('hour')
 plt.ylabel(' P1 (kW)')

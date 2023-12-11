fda818e2f08afd8aee73d7fbf8dc995e0c0766db
Thiago Dantas <89434095+thiagovndantas@users.noreply.github.com>
2023-12-11 17:00:47 -0300

    Update auxiliar.py

--- auxiliar.py
+++ auxiliar.py
@@ -21,11 +21,23 @@ def create_circuit():
 
     # Cria a função para a criação do circuito geral
     files = {
-    "arranjo.txt": "new linecode.arranjo rmatrix=(0.02 | 0.02 0.04 | 0.02 0.04 0.06) xmatrix=(0.48 | 0.48 0.6 | 0.48 0.6 0.69) cmatrix=(-2.51 | 0.69 4.1 | -0.51 0.69 4.1)\n",
+    "arranjo.txt": "new linecode.arranjo \n\
+        rmatrix=(0.02 | 0.02 0.04 | 0.02 0.04 0.06)\n\
+        xmatrix=(0.48 | 0.48 0.6 | 0.48 0.6 0.69) \n\
+        cmatrix=(-2.51 | 0.69 4.1 | -0.51 0.69 4.1)\n",
+
     "fonte.txt": "new circuit.fonte bus1=a basekv=0.22 phases=3\n",
-    "linha1.txt": "new line.linha1 bus1=a bus2=b phases=3 length=0.5 units=km linecode=arranjo\n",
-    "linha2.txt": "new line.linha2 bus1=b bus2=c phases=3 length=0.15 units=km linecode=arranjo\n",
-    "loadshape.txt": "new loadshape.semana",
+    
+    "linhas.txt": "new line.linha1 bus1=a bus2=b phases=3 length=0.5 units=km linecode=arranjo\n\
+        new line.linha2 bus1=b bus2=c phases=3 length=0.15 units=km linecode=arranjo",
+
+    "loadshapes": "new loadshape.semana\n\
+        new loadshape.storagecurve npts=24 interval=1 \n\
+        mult=(0 0 -1 -1 -1 -1 -1 -1 0 0 0 0 0 0 0 0 0 0 0 0.8 0.9 0.94 1 0.94 0)",
+
+    "storage.batery.txt": "new storage.batery phases=3 bus1=c kv=0.22 \n\
+        kwrated=15 kwhrated=60 dispmode=follow daily=storagecurve",
+
     "carga.txt": cargas
     }
 

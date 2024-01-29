f0ab13c7185c77b8d63856f19b73aa70db54b85d
thiagovndantas <thiagovndantas@gmail.com>
2024-01-29 18:34:25 -0300

    ajuste do circuito conforme slide

--- auxiliar.py
+++ auxiliar.py
@@ -13,21 +13,39 @@ def create_circuit():
     num_pvs = int(input("defina a quantidade de cargas com sistemas solares instalados: "))
     sistemas = num_pvs * 5.5
 
+    # Cria um array vazio para concatenar as linhas
+
+    linhas = ""
+
+    # Estrutura de repetição para criação das linhas que conectam as cargas à linha principal
+    for i in range(2, num_cargas + 1):
+        linha_value = f"new line.linha{i} bus1=b bus2=c{i-1} phases=3 length=0.01 units=km linecode=arranjo\n"
+        linhas += linha_value
+
     # Cria um array vazio para concatenar as cargas
     cargas = ""
 
     # Estrutura de repetição para a criação das cargas
     for i in range(1, num_cargas + 1):
-        carga_name = f"carga{i}.txt"
+        carga_name = f"carga{i}"
         kw = random.uniform(50, 60)
         pf = random.uniform(0.8, 1)
-        carga_value = f"new load.{carga_name} phases=3 conn=wye bus1=b kw={kw:.2f} pf={pf:.2f} kv=0.38 daily=default\n"
+        carga_value = f"new load.{carga_name} phases=3 conn=wye bus1=c{i} kw={kw:.2f} pf={pf:.2f} kv=0.38 daily=default\n"
         cargas += carga_value
+
+    # Cria um array vazio para concatenar os pvsysts
+        
+    pvsyst = ""
+
+    # Estrutura de repetição para a criação dos pvsysts
+    for i in range(1, num_pvs + 1):
+        pvsyst_value = f"new pvsystem.pv{i} phases=3 bus1=c{i} kv=0.38 irrad= 1 pmpp=5.5 temperature=26 pf=1 \n\
+        %cutin=0.1 %cutout=0.1 effcurve=myeff p-tcurve=mypvst daily=myirrad tdaily=mytemp\n"
+        pvsyst += pvsyst_value
 
     pvsyst_temp = calcular_media_diaria_por_hora('results.csv')
     pvsyst_pot = calcular_potencia_diaria_por_hora('results.csv',num_pvs)
     pvsyst_irrad = calcular_irradiancia_diaria_por_hora('results.csv')
-
     
     # Cria a função para a criação do circuito geral
     files = {
@@ -38,8 +56,9 @@ def create_circuit():
 
     "fonte.txt": "new circuit.fonte bus1=a basekv=0.380 phases=3\n",
     
-    "linhas.txt": "new line.linha1 bus1=a bus2=b phases=3 length=0.5 units=km linecode=arranjo\n\
-        new line.linha2 bus1=b bus2=c phases=3 length=0.01 units=km linecode=arranjo",
+    "linhas.txt": "new line.linha1 bus1=a bus2=b phases=3 length=0.5 units=km linecode=arranjo\n",
+
+    "linhas_cargas.txt": linhas,
 
     "loadshapes": "new loadshape.semana\n\
     new loadshape.storagecurve npts=24 interval=1 \n\
@@ -57,15 +76,13 @@ def create_circuit():
     "pvsyst_pot.txt": pvsyst_pot,
 
     "mypvsyst.txt": f"new xycurve.mypvsist npts=4 xarray=(0 25 75 100) yarray=(1.2 1 .8 .6) \n\
-    new xycurve.myeff npts=4 xarray=(.1 .2 .4 1) yarray=(.86 .9 .93 .97) \n\
-    new loadshape.myirrad npts=24 interval=1 \n\
-    redirect pvsyst_irrad.txt \n\
-    !mult=(0 0 0 0 0 0 .1 .2 .3 .5 .8 .9 1 1 .99 .9 .7 .4 .1 0 0 0 0 0) \n\
-    new tshape.mytemp npts=24 interval=1 \n\
-    redirect pvsyst_temp.txt \n\
-    !temp=(25 25 25 25 25 25 25 25 35 40 45 50 60 60 55 40 35 30 25 25 25 25 25 25) \n\
-    new pvsystem.pv phases=3 bus1=c kv=0.38 irrad= 1 pmpp={sistemas} temperature=26 pf=1 \n\
-    %cutin=0.1 %cutout=0.1 effcurve=myeff p-tcurve=mypvst daily=myirrad tdaily=mytemp"
+        new xycurve.myeff npts=4 xarray=(.1 .2 .4 1) yarray=(.86 .9 .93 .97) \n\
+        new loadshape.myirrad npts=24 interval=1 \n\
+        redirect pvsyst_irrad.txt \n\
+        new tshape.mytemp npts=24 interval=1 \n\
+        redirect pvsyst_temp.txt\n",
+
+    "mypvsyst_cargas.txt": pvsyst
     }
 
     return files

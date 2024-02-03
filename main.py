e02a1e586c335252e3411230acf1eba0bfed8b37
thiagovndantas <thiagovndantas@gmail.com>
2024-02-03 19:14:57 -0300

    ultimas alteracoes

--- main.py
+++ main.py
@@ -46,28 +46,28 @@ dss_file6 = "simulacao6.dss"
 
 # rodando os arquivos
 dss.text("compile {}".format(dss_file1))
-dss.text("compile {}".format(dss_file2))
-dss.text("compile {}".format(dss_file3))
-dss.text("compile {}".format(dss_file4))
+#dss.text("compile {}".format(dss_file2))
+#dss.text("compile {}".format(dss_file3))
+#dss.text("compile {}".format(dss_file4))
 dss.text("compile {}".format(dss_file5))
-dss.text("compile {}".format(dss_file6))
+#dss.text("compile {}".format(dss_file6))
 
 
 # arquivos gerados pela compilação do código
 
 sim1_power_line = "fonte_Mon_monitor_power_line_sim1_1.csv"  # linha 1 terminal 2
-sim2_power_line = "fonte_Mon_monitor_power_line_sim2_1.csv"  # linha 1 terminal 2
-sim2_power_batery = "fonte_Mon_monitor_power_batery_sim2_1.csv"
-sim3_power_line = "fonte_Mon_monitor_power_line_sim3_1.csv"  # linha 1 terminal 2
-sim3_power_batery = "fonte_Mon_monitor_power_batery_sim3_1.csv"
-sim4_power_line = "fonte_Mon_monitor_power_line_sim4_1.csv"  # linha 1 terminal 2
-sim4_power_batery = "fonte_Mon_monitor_power_batery_sim4_1.csv"
+#sim2_power_line = "fonte_Mon_monitor_power_line_sim2_1.csv"  # linha 1 terminal 2
+#sim2_power_batery = "fonte_Mon_monitor_power_batery_sim2_1.csv"
+#sim3_power_line = "fonte_Mon_monitor_power_line_sim3_1.csv"  # linha 1 terminal 2
+#sim3_power_batery = "fonte_Mon_monitor_power_batery_sim3_1.csv"
+#sim4_power_line = "fonte_Mon_monitor_power_line_sim4_1.csv"  # linha 1 terminal 2
+#sim4_power_batery = "fonte_Mon_monitor_power_batery_sim4_1.csv"
 sim5_power_line = "fonte_Mon_monitor_power_line_sim5_1.csv"  # linha 1 terminal 2
 sim5_power_line2_1 = "fonte_Mon_monitor_power_line2_1_sim5_1.csv"  # linha 2 terminal 1
 sim5_power_line2_2 = "fonte_Mon_monitor_power_line2_2_sim5_1.csv"  # linha 2 terminal 2
-sim6_power_line = "fonte_Mon_monitor_power_line_sim6_1.csv"  # linha 1 terminal 2
-sim6_power_line2 = "fonte_Mon_monitor_power_line2_sim6_1.csv"  # linha 2 terminal 1
-sim6_power_batery = "fonte_Mon_monitor_power_batery_sim6_1.csv"
+#sim6_power_line = "fonte_Mon_monitor_power_line_sim6_1.csv"  # linha 1 terminal 2
+#sim6_power_line2 = "fonte_Mon_monitor_power_line2_sim6_1.csv"  # linha 2 terminal 1
+#sim6_power_batery = "fonte_Mon_monitor_power_batery_sim6_1.csv"
 # sim6_power_pv = "fonte_Mon_monitor_power_pv_sim6_1.csv"
 # sim6_power_load = "fonte_Mon_monitor_power_load_sim6_1.csv"
 
@@ -75,18 +75,18 @@ sim6_power_batery = "fonte_Mon_monitor_power_batery_sim6_1.csv"
 
 
 df_sim1_power_line = pd.read_csv(sim1_power_line)
-df_sim2_power_line = pd.read_csv(sim2_power_line)
-df_sim2_power_batery = pd.read_csv(sim2_power_batery)
-df_sim3_power_line = pd.read_csv(sim3_power_line)
-df_sim3_power_batery = pd.read_csv(sim3_power_batery)
-df_sim4_power_line = pd.read_csv(sim4_power_line)
-df_sim4_power_batery = pd.read_csv(sim4_power_batery)
+#df_sim2_power_line = pd.read_csv(sim2_power_line)
+#df_sim2_power_batery = pd.read_csv(sim2_power_batery)
+#df_sim3_power_line = pd.read_csv(sim3_power_line)
+#df_sim3_power_batery = pd.read_csv(sim3_power_batery)
+#df_sim4_power_line = pd.read_csv(sim4_power_line)
+#df_sim4_power_batery = pd.read_csv(sim4_power_batery)
 df_sim5_power_line = pd.read_csv(sim5_power_line)
 df_sim5_power_line2_1 = pd.read_csv(sim5_power_line2_1)
 df_sim5_power_line2_2 = pd.read_csv(sim5_power_line2_2)
-df_sim6_power_line = pd.read_csv(sim6_power_line)
-df_sim6_power_line2 = pd.read_csv(sim6_power_line2)
-df_sim6_power_batery = pd.read_csv(sim6_power_batery)
+#df_sim6_power_line = pd.read_csv(sim6_power_line)
+#df_sim6_power_line2 = pd.read_csv(sim6_power_line2)
+#df_sim6_power_batery = pd.read_csv(sim6_power_batery)
 # df_sim6_power_pv = pd.read_csv(sim6_power_pv)
 # df_sim6_power_load = pd.read_csv(sim6_power_load)
 
